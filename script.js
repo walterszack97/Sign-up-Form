1778804259bc1d217ff60709deadd37b62fcf30f
zachary w <walterszack97@gmail.com>
2023-02-18 00:20:56 -0500

    Add password and confirm password validation

--- script.js
+++ script.js
@@ -6,4 +6,17 @@ function setTheme() {
 document.querySelector('.checkbox').addEventListener('click', setTheme);
 setTheme();
 
-
+function validate() {
+    let password = document.getElementById('password').value;
+    let confirm_p = document.getElementById('confirm_p').value;
+    if (password != confirm_p) {
+        document.querySelector(".password_validate").textContent = "Passwords do not match";
+        document.getElementById('password').style.borderColor = "red";
+        document.getElementById('confirm_p').style.borderColor = "red";
+        return false;
+    }
+    document.querySelector(".password_validate").textContent = "";
+    document.getElementById('password').style.borderColor = "green";
+        document.getElementById('confirm_p').style.borderColor = "green";
+    return true;
+}
